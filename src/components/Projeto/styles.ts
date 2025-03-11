b9cc7a49c5684994430b078713b5a6887015ae9c
guilherme <guilherme@teste.com>
2025-03-11 17:23:52 -0300

    criando a seção e componentes  de projeto e criando 2versão da responsividade

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  border: 1px solid #c1c1c1;
+  padding: 16px;
+`
+
+export const LinkBotao = styled.a`
+  background-color: #4476bf;
+  color: #fff;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  padding: 8px;
+  margin-top: 24px;
+`
