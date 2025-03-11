18b48bf43db7d9e6222b02d2590102c39ec227fb
guilherme <guilherme@teste.com>
2025-03-11 16:45:25 -0300

    Criando os arquivos de estilo

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/guilhermBM.png" />
+    <Titulo fontSize={20}>Guilherme B. Melo</Titulo>
+  </aside>
+)
+
+export default Sidebar
